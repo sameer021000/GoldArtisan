42046a3e52c1b82e6352fc186b82ff2ddc8e8321
Sameer Shaik <sameer021000@gmail.com>
2025-11-24 15:52:57 +0530

    Modified the content in SplashScreen

--- frontend/src/SplashScreenFolder/SplashScreen.jsx
+++ frontend/src/SplashScreenFolder/SplashScreen.jsx
@@ -17,7 +17,13 @@ const SplashScreen = () => {
     <div id="divId1">
       <div id="divId2">
         <h1 id="h1Id1">Welcome to COMer</h1>
-        <p id="pId1">Crafting unique pieces — join as an artisan</p>
+        <div id="divId3">
+          <h4 id="h4Id1">C-Customized</h4>
+          <h4 id="h4Id2">O-Ornament</h4>
+          <h4 id="h4Id3">M-Maker</h4>
+          <h4 id="h4Id4">That’s what a true COMer is!</h4>
+        </div>
+        <h3 id="h3Id1">A COMer isn’t who you are — it’s what you Create</h3>
       </div>
     </div>
   );
