4071cf22edd731b673a909e9e76c6980f4265412
Sameer Shaik <sameer021000@gmail.com>
2025-11-28 21:17:47 +0530

    Added route for SignInScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SplashScreen from './SplashScreenFolder/SplashScreen';
 import SignUpScreen from './SignUpFolder/SignUpScreen';
 import HomeScreen from './HomeScreenFolder/HomeScreen';
+import SignInScreen from './SignInFolder/SignInScreen';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/SignUpPath" element={<SignUpScreen />} />
+        <Route path="/SignInScreen" element={<SignInScreen/>}/>
         <Route path="/HomeScreen" element={<HomeScreen/>}/>
       </Routes>
     </Router>
