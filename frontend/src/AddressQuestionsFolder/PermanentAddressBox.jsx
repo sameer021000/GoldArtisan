a525973af09b54839595ecae1f81cd8185f90509
Sameer Shaik <sameer021000@gmail.com>
2025-12-15 14:23:07 +0530

    Removing the animation of down and up-arrows

--- frontend/src/AddressQuestionsFolder/PermanentAddressBox.jsx
+++ frontend/src/AddressQuestionsFolder/PermanentAddressBox.jsx
@@ -38,10 +38,7 @@ function PermanentAddressBox({
         </div>
   
         {!isPermanentFormMinimized && (
-          <div id="permanentFormContentWrapper_AddressQuestions"
-            className={isPermanentFormMinimized ? "formCollapsed" : "formExpanded"}
-            aria-hidden={isPermanentFormMinimized}
-          >
+          <div id="permanentFormContentWrapper_AddressQuestions">
             <div id="permanentFormGrid_AddressQuestions">
                 {/* State Dropdown */}
                 <div id="permanentFormGroup_State" className="formGroup_AddressQuestions">
